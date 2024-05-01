7bf41db605eea7eeb32249089f6fb450514babb1
Kappeh <github.dealmaker606@slmail.me>
2024-05-01 19:08:01 +0100

    Help fix tearing of windows for Nvidia cards

--- hosts/neri/nvidia.nix
+++ hosts/neri/nvidia.nix
@@ -1,30 +1,41 @@
 { config, ... }: {
-  # Proprietary Nvidia drivers
-  config.boot.kernelParams = [
-    # Blacklist integrated gpu to prevent hanging on boot
-    # TODO: find source for this beauty
-    "module_blacklist=i915"
-    # Workaround to remove "ghost display" (https://bbs.archlinux.org/viewtopic.php?id=288009)
-    # Leaving disabled because it seems to be a Wayland thing and
-    # it removes console output during boot, including the LUKS prompt
-    #"initcall_blacklist=simpledrm_platform_driver_init"
-    #"nvidia-drm.modeset=1"
-  ];
+  config = {
+    # Proprietary Nvidia drivers
+    boot.kernelParams = [
+      # Blacklist integrated gpu to prevent hanging on boot
+      # TODO: find source for this beauty
+      "module_blacklist=i915"
+      # Workaround to remove "ghost display" (https://bbs.archlinux.org/viewtopic.php?id=288009)
+      # Leaving disabled because it seems to be a Wayland thing and
+      # it removes console output during boot, including the LUKS prompt
+      #"initcall_blacklist=simpledrm_platform_driver_init"
+      #"nvidia-drm.modeset=1"
+    ];
 
-  config.services.xserver.videoDrivers = [ "nvidia" ];
+    services.xserver = {
+      videoDrivers = [ "nvidia" ];
 
-  config.hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true; # Required for steam
+      # This helps fix tearing of windows for Nvidia cards
+      screenSection = ''
+        Option       "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+        Option       "AllowIndirectGLXProtocol" "off"
+        Option       "TripleBuffer" "on"
+      '';
     };
-    nvidia = {
-      modesetting.enable = true; # Required for native resolution in TTY
-      powerManagement.enable = true;
-      open = true;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable; # stable, production, latest, beta or vulkan_beta
+
+    hardware = {
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true; # Required for steam
+      };
+      nvidia = {
+        modesetting.enable = true; # Required for native resolution in TTY
+        powerManagement.enable = true;
+        open = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable; # stable, production, latest, beta or vulkan_beta
+      };
     };
   };
 }
