04902881f1244fdd2ef72346f24cbce5e6e98f30
Kappeh <github.dealmaker606@slmail.me>
2024-06-02 16:34:07 +0100

    Make pipewire primary sound server

--- hosts/neri/configuration.nix
+++ hosts/neri/configuration.nix
@@ -98,17 +98,24 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    audio.enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
     jack.enable = true;
 
-    wireplumber.extraConfig = {
-      "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "a2dp_sink" ];
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [ "a2dp_sink" ];
+        };
       };
     };
   };
