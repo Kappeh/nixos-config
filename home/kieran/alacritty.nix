330265338b453e88d93349b412813a75f40896d1
Kappeh <github.dealmaker606@slmail.me>
2024-09-10 14:53:18 +0100

    Add padding and adjust colours of alacritty

--- home/kieran/alacritty.nix
+++ home/kieran/alacritty.nix
@@ -4,12 +4,24 @@
 
     settings = {
       window = {
-        opacity = 0.8;
+        opacity = 0.6;
+        padding = { x = 20; y = 20; };
+        # dynamic_padding = true;
+      };
+      scrolling = {
+        multiplier = 1;
+        history = 10000;
       };
       cursor.style = {
         shape = "Beam";
         blinking = "On";
       };
+      colors.primary = {
+        background = "#1b2230";
+        foreground = "#c5c8c6";
+        dim_foreground = "#707880";
+      };
+      font.size = 10;
     };
   };
 }
