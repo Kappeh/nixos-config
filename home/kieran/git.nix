03240545e5c73d75f43bba47cbce029c7b530b35
Kappeh <github.dealmaker606@slmail.me>
2024-09-30 00:54:32 +0100

    Enable git delta

--- home/kieran/git.nix
+++ home/kieran/git.nix
@@ -1,8 +1,14 @@
 { ... }: {
   programs.git = {
     enable = true;
-
     userName = "Kappeh";
     userEmail = "github.dealmaker606@slmail.me";
+
+    delta = {
+      enable = true;
+      options = {
+        side-by-side = true;
+      };
+    };
   };
 }
