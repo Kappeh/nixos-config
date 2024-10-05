b19921b5e663f56b894382362def179b4f01f7aa
Kappeh <github.dealmaker606@slmail.me>
2024-10-05 13:09:51 +0100

    Add zsh-nix-shell, allowing use of zsh in nix-shell

--- home/kieran/zsh.nix
+++ home/kieran/zsh.nix
@@ -15,6 +15,19 @@
       path = "${config.xdg.dataHome}/zsh/zsh_history";
     };
 
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+
     oh-my-zsh = {
       enable = true;
 
