b2710f873f2b0cdb814cbfac5ce7cf9bf6ff4a00
Kappeh <github.dealmaker606@slmail.me>
2024-10-30 14:19:40 +0000

    Remove redundant services.xserver.enable option

--- hosts/neri/configuration.nix
+++ hosts/neri/configuration.nix
@@ -49,11 +49,6 @@
     useXkbConfig = true; # use xkb.options in tty.
   };
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-  };
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "gb";
