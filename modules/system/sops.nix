e69078d997d9a1f6cfcbe7e73e45448423bc5fe9
Kappeh <github.dealmaker606@slmail.me>
2024-12-22 16:28:49 +0000

    Fix evaluation error where sops required keyfile external to config

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -2,7 +2,7 @@
   config.sops = {
     defaultSopsFile = ../../sops/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = /persist/system/root/.config/sops/age/keys.txt;
+    age.keyFile = "/persist/system/root/.config/sops/age/keys.txt";
     secrets."users/kieran/hashedPassword".neededForUsers = true;
   };
 }
