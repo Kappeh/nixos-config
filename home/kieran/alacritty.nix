07d9676a8d1e78464e8e7cfa576f58b0f2de6264
Kappeh <github.dealmaker606@slmail.me>
2025-03-06 10:49:44 +0000

    Update theme of alacritty to be consistent with the rest of the system

--- home/kieran/alacritty.nix
+++ home/kieran/alacritty.nix
@@ -4,7 +4,7 @@
 
     settings = {
       window = {
-        opacity = 0.8;
+        opacity = 1.0;
         padding = { x = 20; y = 20; };
         # dynamic_padding = true;
       };
@@ -16,10 +16,33 @@
         shape = "Beam";
         blinking = "On";
       };
-      colors.primary = {
-        background = "#1b2230";
-        foreground = "#c5c8c6";
-        dim_foreground = "#707880";
+      colors = {
+        primary = {
+          foreground = "#b1b9f1";
+          background = "#14142c";
+          dim_foreground = "#44447e";
+          bright_foreground = "#9e9edf";
+        };
+        normal = {
+          black = "#252549";
+          red = "#ff5370";
+          green = "#58dd80";
+          yellow = "#f0af74";
+          blue = "#537bd2";
+          magenta = "#8473de";
+          cyan = "#45c8c5";
+          white = "#747495";
+        };
+        bright = {
+          black = "#44447e";
+          red = "#e97a7a";
+          green = "#8af193";
+          yellow = "#d6b295";
+          blue = "#90afff";
+          magenta = "#9f96eb";
+          cyan = "#8cdac9";
+          white = "#d8d8ff";
+        };
       };
       font.size = 10;
     };
