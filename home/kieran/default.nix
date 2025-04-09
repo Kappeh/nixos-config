304eb1253a9130d4067018bda5ed403376595239
Kappeh <github.dealmaker606@slmail.me>
2025-04-09 06:23:20 +0100

    Set librewolf as default web browser

--- home/kieran/default.nix
+++ home/kieran/default.nix
@@ -102,6 +102,11 @@
         enable = true;
         defaultApplications = {
           "inode/directory" = [ "pcmanfm.desktop" ];
+          "text/html" = "librewolf.desktop";
+          "x-scheme-handler/http" = "librewolf.desktop";
+          "x-scheme-handler/https" = "librewolf.desktop";
+          "x-scheme-handler/about" = "librewolf.desktop";
+          "x-scheme-handler/unknown" = "librewolf.desktop";
         };
       };
     };
