5e4996596ced83e5cce4f1ced55bfa880e75a9ba
Kappeh <github.dealmaker606@slmail.me>
2025-06-16 10:32:57 +0100

    Add systemd service to update work rota hourly

--- hosts/ciela/configuration.nix
+++ hosts/ciela/configuration.nix
@@ -5,6 +5,7 @@
   imports = [
     ../../modules/system/programs/docker.nix
     ../../modules/system/services/illegal_crime_mc/default.nix
+    ../../modules/system/services/update_rota/default.nix
     ../common.nix
     # ./mc_server_backup/default.nix
     # ./update_mc_server_blacklist/default.nix
---
+++ modules/system/services/update_rota/update_rota.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker compose -f /services/rota_scraper/docker-compose.yml up -d update_rota
