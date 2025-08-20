f46ea2fdd346f7981ff7eed82f7639bc8488d4be
Kappeh <github.dealmaker606@slmail.me>
2025-08-20 13:01:03 +0100

    Persist libreoffice config files

--- modules/home/applications/media/libreoffice.nix
+++ modules/home/applications/media/libreoffice.nix
@@ -1,7 +1,11 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, osConfig, pkgs, ... }: {
   options.myModules.applications.media.libreoffice.enable = lib.mkEnableOption "Enable LibreOffice";
 
   config = lib.mkIf config.myModules.applications.media.libreoffice.enable {
     home.packages = [ pkgs.libreoffice-qt ];
+
+    home.persistence = lib.mkIf osConfig.myModules.core.impermanence.enable {
+      "/persist/home/${config.home.username}".directories = [ ".config/libreoffice" ];
+    };
   };
 }
