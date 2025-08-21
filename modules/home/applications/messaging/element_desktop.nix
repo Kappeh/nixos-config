974fbe53ec4383c461998ab71e3a0519f8f860d7
Kappeh <github.dealmaker606@slmail.me>
2025-08-21 16:49:33 +0100

    Add link to track issue affecting Element Desktop

--- modules/home/applications/messaging/element_desktop.nix
+++ modules/home/applications/messaging/element_desktop.nix
@@ -1,6 +1,13 @@
 { config, lib, osConfig, pkgs, ... }: {
   options.myModules.applications.messaging.element_desktop.enable = lib.mkEnableOption "Enable Element";
 
+  # Does not work due to electron bug
+  #
+  # Use Secret Service API in DBus to attempt to autodiscover secret provider
+  # https://github.com/electron/electron/issues/47436
+  # Safestorage detection fails on linux outside of predefined desktop environments
+  # https://github.com/electron/electron/issues/39789
+
   config = lib.mkIf config.myModules.applications.messaging.element_desktop.enable {
     home = {
       packages = [ pkgs.element-desktop ];
