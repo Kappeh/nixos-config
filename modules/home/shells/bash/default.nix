10dfb9a92240231b4ef7f90981bbaed802bfccba
Kappeh <github.dealmaker606@slmail.me>
2025-09-05 18:32:19 +0100

    Fix bash_history location

--- modules/home/shells/bash/default.nix
+++ modules/home/shells/bash/default.nix
@@ -3,7 +3,7 @@
     programs.bash = {
       enable = true;
 
-      historyFile = "~/.bash_history";
+      historyFile = "${config.home.homeDirectory}/.bash_history";
       historyFileSize = 10000;
       historySize = 1000;
 
