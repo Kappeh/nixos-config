4c9a98ce697c7dcb6feeea942c8191f703ff25e0
Kappeh <github.dealmaker606@slmail.me>
2025-11-11 04:16:21 +0000

    Add act_runner user to leaf

--- hosts/leaf/services/gitea.nix
+++ hosts/leaf/services/gitea.nix
@@ -1,19 +1,40 @@
 { pkgs, ... }: {
   config.users = {
-    users.gitea = {
-      uid = 2000;
-      group = "gitea";
-      isNormalUser = false;
-      isSystemUser = true;
-      useDefaultShell = false;
-      shell = pkgs.shadow;
+    users = {
+      gitea = {
+        uid = 2000;
+        group = "gitea";
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
+      act_runner = {
+        uid = 2001;
+        group = "act_runner";
+        extraGroups = [ "docker" ];
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
     };
-    groups.gitea = {
-      gid = 2000;
-      members = [
-        "gitea"
-        "kieran"
-      ];
+    groups = {
+      gitea = {
+        gid = 2000;
+        members = [
+          "gitea"
+          "kieran"
+        ];
+      };
+      act_runner = {
+        gid = 2001;
+        members = [
+          "act_runner"
+          "kieran"
+        ];
+      };
     };
   };
 }
+
