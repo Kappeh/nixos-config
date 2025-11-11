fce936ca9ec87b76e4add2835be3abab4718890a
Kappeh <github.dealmaker606@slmail.me>
2025-11-11 19:40:26 +0000

    Add gitea_deploy user

--- hosts/leaf/services/gitea.nix
+++ hosts/leaf/services/gitea.nix
@@ -18,6 +18,19 @@
         useDefaultShell = false;
         shell = pkgs.shadow;
       };
+      gitea_deploy = {
+        uid = 2002;
+        group = "gitea_deploy";
+        extraGroups = [ "docker" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINj0MBPdFYe4Rx5lxtWQ1lWpl2y6amHfdPX1TcbeY43j gitea_deploy"
+        ];
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.bash;
+        packages = [ pkgs.git ];
+      };
     };
     groups = {
       gitea = {
@@ -31,6 +44,13 @@
         gid = 2001;
         members = [
           "act_runner"
+          "kieran"
+        ];
+      };
+      gitea_deploy = {
+        gid = 2002;
+        members = [
+          "gitea_deploy"
           "kieran"
         ];
       };
