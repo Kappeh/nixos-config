3f59e6dac3717b511d6f6e022606a9419c9f2b01
Kappeh <github.dealmaker606@slmail.me>
2025-11-17 17:03:54 +0000

    Add ntfy_deploy user

--- hosts/leaf/services/default.nix
+++ hosts/leaf/services/default.nix
@@ -1,14 +1,22 @@
 { ... }: {
   imports = [
     ./gitea.nix # 2000, 2001
-    ./ntfy.nix  # 2002
+    ./ntfy.nix  # 2002, 2003
   ];
+
+  config.users.groups.deploy = {
+    name = "deploy";
+    gid = 1999;
+    members = [
+      "ntfy_deploy"
+    ];
+  };
 
   config.environment.persistence."/persist/system".directories = [
     {
       directory = "/services";
       user = "kieran";
-      group = "kieran";
+      group = "deploy";
       mode = "u=rwx,g=rwx,o=";
     }
   ];
--- hosts/leaf/services/ntfy.nix
+++ hosts/leaf/services/ntfy.nix
@@ -1,20 +1,45 @@
 { inputs, pkgs, ... }: {
   config.users = {
-    users.ntfy_server = {
-      uid = 2002;
-      group = "ntfy_server";
-      isNormalUser = false;
-      isSystemUser = true;
-      useDefaultShell = false;
-      shell = pkgs.shadow;
+    users = {
+      ntfy_server = {
+        uid = 2002;
+        group = "ntfy_server";
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
+      ntfy_deploy = {
+        uid = 2003;
+        group = "ntfy_deploy";
+        extraGroups = [
+          "deploy"
+          "docker"
+        ];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHIC6onCBF+F/d0rwByTms8kOGHKFH/Q/vw/EFEPPpoe ntfy_deploy_ed25519" ];
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.bash;
+      };
     };
-    groups.ntfy_server = {
-      name = "ntfy_server";
-      gid = 2002;
-      members = [
-        "ntfy_server"
-        "kieran"
-      ];
+    groups = {
+      ntfy_server = {
+        name = "ntfy_server";
+        gid = 2002;
+        members = [
+          "ntfy_server"
+          "kieran"
+        ];
+      };
+      ntfy_deploy = {
+        name = "ntfy_deploy";
+        gid = 2003;
+        members = [
+          "ntfy_deploy"
+          "kieran"
+        ];
+      };
     };
   };
 }
