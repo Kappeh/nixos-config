47dc0d8f749749d249b5e874411cd979519819ea
Kappeh <github.dealmaker606@slmail.me>
2025-12-05 17:56:48 +0000

    Pin Nvidia driver version

--- modules/system/core/nvidia.nix
+++ modules/system/core/nvidia.nix
@@ -27,10 +27,23 @@
         finegrained = false;
       };
 
-      open = true;
+      # Disabled temporarily because of a bug in the open nvidia driver derivation for version 580.76.08
+      # open = true;
+      open = false;
       nvidiaSettings = true;
 
-      package = config.boot.kernelPackages.nvidiaPackages.beta; # stable, production, latest, beta or vulkan_beta
+      # Pinned due to a bug in version 580.105.08
+      # https://forums.developer.nvidia.com/t/undetected-ultrawide-resolution-upon-upgrade-to-driver-version-580-105-08/350969
+      # package = config.boot.kernelPackages.nvidiaPackages.beta; # stable, production, latest, beta or vulkan_beta
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "580.76.05";
+        sha256_64bit = "sha256-IZvmNrYJMbAhsujB4O/4hzY8cx+KlAyqh7zAVNBdl/0=";
+        sha256_aarch64 = "sha256-IZvmNrYJMbAhsujB4O/4hzY8cx+KlAyqh7zAVNBdl/0=";
+        openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        persistencedSha256 = lib.fakeSha256;
+      };
     };
   };
 }
+
