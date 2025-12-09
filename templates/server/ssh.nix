06ed89ad33bbaf60c9b530ea918cd3a2f26ac76e
Kappeh <github.dealmaker606@slmail.me>
2025-12-09 01:50:43 +0000

    Remove unused known hosts and update ssh host key location

--- templates/server/ssh.nix
+++ templates/server/ssh.nix
@@ -1,26 +1,19 @@
 { config, lib, ... }: {
-  config = {
-    services.openssh = {
-      enable = true;
+  config.services.openssh = {
+    enable = true;
 
-      settings = {
-        PermitRootLogin = "no";
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-      };
-
-      hostKeys = [
-        {
-          comment = "leaf_host_key_ed25519";
-          path = "/persist/system/etc/ssh/leaf_host_key_ed25519";
-          type = "ed25519";
-        }
-      ];
-
-      knownHosts."git.illegal-crime.org" = {
-        hostNames = [ "[git.illegal-crime.org]:2222" ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILTrGImrwi6JAv2JSf5hVKU3SBh/PcA0+Zmwl4f2+3h3 gitea_host_key";
-      };
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
     };
+
+    hostKeys = [
+      {
+        comment = "host_key_25519";
+        path = "/persist/system/etc/ssh/host_key_ed25519";
+        type = "ed25519";
+      }
+    ];
   };
 }
