e9bad6886d811236d37d513db21c1e0480c9c27a
Kappeh <github.dealmaker606@slmail.me>
2026-01-02 20:18:09 +0000

    Add network share to duplicati service

--- templates/server/services/duplicati.nix
+++ templates/server/services/duplicati.nix
@@ -1,20 +1,32 @@
 { pkgs, ... }: {
-  config.users = {
-    users.duplicati = {
-      uid = 2011;
-      group = "duplicati";
-      isNormalUser = false;
-      isSystemUser = true;
-      useDefaultShell = false;
-      shell = pkgs.shadow;
-    };
-    groups.duplicati = {
-      name = "duplicati";
-      gid = 2011;
-      members = [
-        "duplicati"
-        "kieran"
+  config = {
+    fileSystems."/mnt/duplicati_backup_1" = {
+      device = "omv.home.kappeh.org:/export/duplicati-backup-1";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=600"
       ];
+    };
+
+    users = {
+      users.duplicati = {
+        uid = 2011;
+        group = "duplicati";
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
+      groups.duplicati = {
+        name = "duplicati";
+        gid = 2011;
+        members = [
+          "duplicati"
+          "kieran"
+        ];
+      };
     };
   };
 }
