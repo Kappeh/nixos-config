d822376f06efc1c13676436ab30d3a6fc5f92f1e
Kappeh <github.dealmaker606@slmail.me>
2026-01-13 12:00:56 +0000

    Disable workqueues and add discord to luks devices

--- hosts/neri/configuration.nix
+++ hosts/neri/configuration.nix
@@ -36,6 +36,17 @@
       };
     };
 
+    boot.initrd.luks.devices = {
+      "crypt1" = {
+        allowDiscards = true;
+        bypassWorkqueues = true;
+      };
+      "crypt2" = {
+        allowDiscards = true;
+        bypassWorkqueues = true;
+      };
+    };
+
     services.btrfs.autoScrub = {
       enable = true;
       interval = "monthly";
