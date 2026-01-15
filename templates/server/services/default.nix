32546a0065954c87440bbf2222dc5701efb8a2f0
Kappeh <github.dealmaker606@slmail.me>
2026-01-15 19:45:34 +0000

    Add minecraft_server service

--- templates/server/services/default.nix
+++ templates/server/services/default.nix
@@ -1,19 +1,20 @@
 { pkgs, ... }: {
   imports = [
-    ./gitea.nix           # 2000, 2001
-    ./ntfy.nix            # 2002
-    ./loki.nix            # 2003
-    ./mailrise.nix        # 2004
-    ./grafana.nix         # 2005
-    ./portainer.nix       # 2006
-    ./jellyfin.nix        # 2007
-    ./ente.nix            # 2008
-    ./uptime_kuma.nix     # 2009
-    ./syncthing.nix       # 2010
-    ./duplicati.nix       # 2011
-    ./navidrome.nix       # 2012
-    ./namecheap_ddns.nix  # 2013
-    ./docker_registry.nix # 2014
+    ./gitea.nix            # 2000, 2001
+    ./ntfy.nix             # 2002
+    ./loki.nix             # 2003
+    ./mailrise.nix         # 2004
+    ./grafana.nix          # 2005
+    ./portainer.nix        # 2006
+    ./jellyfin.nix         # 2007
+    ./ente.nix             # 2008
+    ./uptime_kuma.nix      # 2009
+    ./syncthing.nix        # 2010
+    ./duplicati.nix        # 2011
+    ./navidrome.nix        # 2012
+    ./namecheap_ddns.nix   # 2013
+    ./docker_registry.nix  # 2014
+    ./minecraft_server.nix # 2015
   ];
 
   config.users = {
---
+++ templates/server/services/minecraft_server.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }: {
+  config = {
+    fileSystems."/mnt/minecraft_server" = {
+      device = "omv.home.kappeh.org:/export/minecraft-server";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=600"
+      ];
+    };
+
+    users = {
+      users.minecraft_server = {
+        uid = 2015;
+        group = "minecraft_server";
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
+      groups.minecraft_server = {
+        name = "minecraft_server";
+        gid = 2015;
+        members = [
+          "minecraft_server"
+          "kieran"
+        ];
+      };
+    };
+  };
+}
