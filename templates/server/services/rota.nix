5a1d2db7aa9101e9a5a09c9cc96c5bb4a226d0ee
Kappeh <github.dealmaker606@slmail.me>
2026-01-29 22:14:01 +0000

    Add systemd units to rota service

--- templates/server/services/rota.nix
+++ templates/server/services/rota.nix
@@ -1,20 +1,41 @@
 { pkgs, ... }: {
-  config.users = {
-    users.rota = {
-      uid = 2018;
-      group = "rota";
-      isNormalUser = false;
-      isSystemUser = true;
-      useDefaultShell = false;
-      shell = pkgs.shadow;
+  config = {
+    users = {
+      users.rota = {
+        uid = 2018;
+        group = "rota";
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
+      groups.rota = {
+        name = "rota";
+        gid = 2018;
+        members = [
+          "rota"
+          "kieran"
+        ];
+      };
     };
-    groups.rota = {
-      name = "rota";
-      gid = 2018;
-      members = [
-        "rota"
-        "kieran"
-      ];
+    systemd = {
+      services."rota_scraper" = {
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+          WorkingDirectory = "/services/rota";
+          ExecStart = "/run/current-system/sw/bin/docker compose up scraper";
+          Environment = "PATH=/run/current-system/sw/bin";
+        };
+      };
+      timers."rota_scraper" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "hourly";
+          Persistent = true;
+          Unit = "rota_scraper.service";
+        };
+      };
     };
   };
 }
