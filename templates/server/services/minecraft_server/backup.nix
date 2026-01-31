72edda3e9af7a173f4f7ee1757ccda92d8e909f0
Kappeh <github.dealmaker606@slmail.me>
2026-01-31 00:01:18 +0000

    Add minecraft_server_mark_daily_backup systemd service

--- templates/server/services/minecraft_server/backup.nix
+++ templates/server/services/minecraft_server/backup.nix
@@ -17,5 +17,23 @@
         Unit = "minecraft_server_backup.service";
       };
     };
+
+    services."minecraft_server_mark_daily_backup" = {
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        WorkingDirectory = "/services/minecraft_server";
+        ExecStart = "/services/minecraft_server/scripts/mark_daily_backup.sh";
+        Environment = "PATH=/run/current-system/sw/bin";
+      };
+    };
+    timers."minecraft_server_mark_daily_backup" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+        Unit = "minecraft_server_mark_daily_backup.service";
+      };
+    };
   };
 }
