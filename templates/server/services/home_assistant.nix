de8d304867257b9a5c42ce4827fbc5229c8badd2
Kappeh <github.dealmaker606@slmail.me>
2026-02-10 21:36:55 +0000

    Add home_assistant_1 nfs share

--- templates/server/services/home_assistant.nix
+++ templates/server/services/home_assistant.nix
@@ -1,20 +1,32 @@
 { pkgs, ... }: {
-  config.users = {
-    users.home_assistant = {
-      uid = 2019;
-      group = "home_assistant";
-      isNormalUser = false;
-      isSystemUser = true;
-      useDefaultShell = false;
-      shell = pkgs.shadow;
-    };
-    groups.home_assistant = {
-      name = "home_assistant";
-      gid = 2019;
-      members = [
-        "home_assistant"
-        "kieran"
+  config = {
+    fileSystems."/mnt/home_assistant_1" = {
+      device = "omv.home.kappeh.org:/export/home-assistant-1";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=600"
       ];
+    };
+
+    users = {
+      users.home_assistant = {
+        uid = 2019;
+        group = "home_assistant";
+        isNormalUser = false;
+        isSystemUser = true;
+        useDefaultShell = false;
+        shell = pkgs.shadow;
+      };
+      groups.home_assistant = {
+        name = "home_assistant";
+        gid = 2019;
+        members = [
+          "home_assistant"
+          "kieran"
+        ];
+      };
     };
   };
 }
