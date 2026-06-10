2f5417a7ec4bdb77c69a11867b546337c07ab2bb
Kappeh <github.dealmaker606@slmail.me>
2026-06-10 16:15:52 +0100

    users: cleanup config for kieran

--- modules/users/kieran.nix
+++ modules/users/kieran.nix
@@ -2,62 +2,60 @@
   options.myModules.users.normal.kieran.enable = lib.mkEnableOption "Enable kieran user and group";
 
   config = lib.mkIf config.myModules.users.normal.kieran.enable {
-    users = {
-      users.kieran = {
-        name = "kieran";
-        uid = 1000;
-        group = "kieran";
-        isNormalUser = true;
-        hashedPasswordFile = config.sops.secrets."users/kieran/hashedPassword".path;
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCUJsStgjTCObc7BrzoGDE3tj633SbghefFM2wk20gX local" ];
-        shell = pkgs.zsh;
-        useDefaultShell = true;
-        extraGroups = builtins.concatLists [
-          [
-            "users"
+    users.users.kieran = {
+      uid = 1000;
 
-            "audio"           # Enable audio devices for user
-            "video"           # Enable video devices for user
-            "wheel"           # Enable `sudo` for user
+      name = "kieran";
+      group = "kieran";
+      extraGroups = builtins.concatLists [
+        [
+          "users"
+          "audio"           # Enable audio devices for user
+          "video"           # Enable video devices for user
+          "wheel"           # Enable `sudo` for user
+          "wireshark"
+        ]
+        # Allow gamemode user daemon to change CPU governor or niceness
+        (lib.optional config.myModules.applications.gaming.gamemode.enable "gamemode")
+        # Grant password-less access to the RW daemon socket
+        (lib.optional config.myModules.virtualisation.qemu.enable "libvirtd")
+      ];
 
-            "wireshark"
-          ]
-          # Allow gamemode user daemon to change CPU governor or niceness
-          (lib.optional config.myModules.applications.gaming.gamemode.enable "gamemode")
-          # Grant password-less access to the RW daemon socket
-          (lib.optional config.myModules.virtualisation.qemu.enable "libvirtd")
-        ];
-      };
-      groups.kieran = {
-        name = "kieran";
-        gid = 1000;
-        members = [ "kieran" ];
-      };
+      hashedPasswordFile = config.sops.secrets."users/kieran/hashedPassword".path;
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCUJsStgjTCObc7BrzoGDE3tj633SbghefFM2wk20gX local" ];
+
+      isNormalUser = true;
+      isSystemUser = false;
+
+      shell = pkgs.zsh;
+      useDefaultShell = true;
     };
 
-    home-manager.users.kieran = { config, osConfig, lib, inputs, ...}: {
+    users.groups.kieran = {
+      gid = 1000;
+
+      name = "kieran";
+      members = [ "kieran" ];
+    };
+
+    home-manager.users.kieran = { config, osConfig, ...}: {
       imports = [
         ../home/default.nix
       ];
+
       config = {
-        myModules = {
-          applications = {
-            enable = osConfig.myModules.applications.enable;
-
-            # Build can fail due to race condition in libsecret test
-            # https://gitlab.gnome.org/GNOME/libsecret/-/issues/80
-            # media.davinci_resolve.enable = false;
-          };
-          core.enable = true;
-          desktop.enable = osConfig.myModules.desktop.enable;
-          shells.enable = true;
-          tools.enable = true;
-        };
-
         home = {
           username = "kieran";
           homeDirectory = "/home/${config.home.username}";
           stateVersion = "24.05";
+        };
+
+        myModules = {
+          applications.enable = osConfig.myModules.applications.enable;
+          core.enable = true;
+          desktop.enable = osConfig.myModules.desktop.enable;
+          shells.enable = true;
+          tools.enable = true;
         };
 
         home.persistence."/backup".directories = [
--- templates/server/kieran.nix
+++ templates/server/kieran.nix
@@ -1,29 +1,41 @@
-{ config, inputs, pkgs, ... }: {
+{ config, pkgs, ... }: {
   config = {
     users.users.kieran = {
       uid = 1000;
+
       name = "kieran";
       group = "kieran";
       extraGroups = [ "wheel" ];
+
       hashedPasswordFile = config.sops.secrets."users/kieran/hashedPassword".path;
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCUJsStgjTCObc7BrzoGDE3tj633SbghefFM2wk20gX local" ];
+
       isNormalUser = true;
       isSystemUser = false;
+
       shell = pkgs.zsh;
       useDefaultShell = true;
     };
 
     users.groups.kieran = {
       gid = 1000;
+
+      name = "kieran";
       members = [ "kieran" ];
     };
 
-    home-manager.users.kieran = { ... }: {
+    home-manager.users.kieran = { config, ... }: {
       imports = [
         ../../modules/home/default.nix
       ];
 
       config = {
+        home = {
+          username = "kieran";
+          homeDirectory = "/home/${config.home.username}";
+          stateVersion = "24.05";
+        };
+
         myModules = {
           core = {
             sops.enable = true;
@@ -40,12 +52,6 @@
             playerctl.enable = false;
             pulsemixer.enable = false;
           };
-        };
-
-        home = {
-          username = "kieran";
-          homeDirectory = "/home/kieran";
-          stateVersion = "24.05";
         };
       };
     };
