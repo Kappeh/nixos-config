ceee2ced67e5f22bcf5365458bfc27d6a683f59c
Kappeh <github.dealmaker606@slmail.me>
2026-08-26 16:10:16 +0100

    neri: remove unused filesystems

--- hosts/neri/filesystems.nix
+++ hosts/neri/filesystems.nix
@@ -5,11 +5,11 @@
     fileSystems = {
       "/".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
       "/backup".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
-      "/backups".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
+      # "/backups".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
       "/persist".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
       "/nix".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
       "/raid_root".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
-      "/snapshots".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
+      # "/snapshots".options = [ "noatime" "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
       "/swap".options = [ "noatime" "discard=async" "ssd" "space_cache=v2" ];
       "/var/log" = {
         options = [ "compress=zstd:1" "discard=async" "ssd" "space_cache=v2" ];
